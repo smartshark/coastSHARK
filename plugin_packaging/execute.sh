642322f88602d95c87cb66f45516ebed159265e3
atrautsch <alexander.trautsch@cs.uni-goettingen.de>
2020-01-02 11:52:25 +0100

    add slurm environment variable for temp job folder

--- plugin_packaging/execute.sh
+++ plugin_packaging/execute.sh
@@ -3,6 +3,7 @@
 PLUGIN_PATH=$3
 REPOSITORY_PATH=$2
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+RAMDISK_PATH="/dev/shm/$NEW_UUID"
 
 # https://stackoverflow.com/questions/24597818/exit-with-error-message-in-bash-oneline
 function error_exit {
@@ -10,12 +11,17 @@ function error_exit {
     exit "${2:-1}"  ## Return a code specified by $2 or 1 by default.
 }
 
-cp -R $REPOSITORY_PATH "/dev/shm/$NEW_UUID" || error_exit "error copy to ramdisk"
+# in case of slurm we use the provided jobid and path
+if [ ! -z "$SLURM_JOB_ID" ]; then
+    RAMDISK_PATH="/dev/shm/jobs/$SLURM_JOB_ID"
+fi
 
-cd "/dev/shm/$NEW_UUID" || error_exit "error cd to ramdisk"
+rsync -q -r $REPOSITORY_PATH/ $RAMDISK_PATH || error_exit "error copy to ramdisk"
+
+cd $RAMDISK_PATH || error_exit "error cd to ramdisk"
 git checkout -f --quiet $1 || error_exit "error checkout"
 
-COMMAND="python3.5 $PLUGIN_PATH/smartshark_plugin.py --repository_url ${4} --project_name ${5} -DB ${8} -H ${9} -p ${10} -r ${1} -i /dev/shm/$NEW_UUID/"
+COMMAND="python3.5 $PLUGIN_PATH/smartshark_plugin.py --repository_url ${4} --project_name ${5} -DB ${8} -H ${9} -p ${10} -r ${1} -i $RAMDISK_PATH"
 
     
 if [ ! -z ${6+x} ] && [ ${6} != "None" ]; then
@@ -46,12 +52,15 @@ $COMMAND
 
 # if folder does not exist log it to stderr only and exit later with 1
 MISSING=""
-if [ ! -d "/dev/shm/$NEW_UUID/.git" ]; then
+if [ ! -d "$RAMDISK_PATH/.git" ]; then
     (>&2 echo ".git folder not found!")
     MISSING="yes"
 fi
 
-rm -rf "/dev/shm/$NEW_UUID"
+# if we are on slurm this is automated (and we have no permission to remove the folder anyway) 
+if [ -z "$SLURM_JOB_ID" ]; then
+    rm -rf $RAMDISK_PATH
+fi
 
 if [ ! -z "$MISSING" ]; then
     exit 1
